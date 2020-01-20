279331c25fd4fb619c1a0ecd9ba9eb48f921109a
Jordan Barrios <50182887+JordanTBarrios@users.noreply.github.com>
2020-01-20 15:22:41 -0500

    Delete signup.js

--- signup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("aContinue").onclick = function () {
-    'use strict';
-    try {
-        location.href = "/~jbarrios2017/p7/login.php";
-    } catch (ex) {
-        console.log(ex.name + ": " + ex.message);
-    }
-};
