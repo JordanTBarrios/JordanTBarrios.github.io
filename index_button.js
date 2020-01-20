305b3bc79775fb9ad7fdbbfd0d077ad21bdd103c
Jordan Barrios <50182887+JordanTBarrios@users.noreply.github.com>
2020-01-20 15:28:26 -0500

    Add files via upload
    new initial commit

---
+++ index_button.js
@@ -0,0 +1,56 @@
+document.getElementById("projectbtn1").onclick = function () {
+    'use strict';
+    try {
+        location.href = "/~jbarrios2017/p1";
+    } catch (ex) {
+        console.log(ex.name + ": " + ex.message);
+    }
+};
+document.getElementById("projectbtn2").onclick = function () {
+    'use strict';
+    try {
+        location.href = "/~jbarrios2017/p2";
+    } catch (ex) {
+        console.log(ex.name + ": " + ex.message);
+    }
+};
+document.getElementById("projectbtn3").onclick = function () {
+    'use strict';
+    try {
+        location.href = "/~jbarrios2017/p3";
+    } catch (ex) {
+        console.log(ex.name + ": " + ex.message);
+    }
+};
+document.getElementById("projectbtn4").onclick = function () {
+    'use strict';
+    try {
+        location.href = "/~jbarrios2017/p4";
+    } catch (ex) {
+        console.log(ex.name + ": " + ex.message);
+    }
+};
+document.getElementById("projectbtn5").onclick = function () {
+    'use strict';
+    try {
+        location.href = "/~jbarrios2017/p5";
+    } catch (ex) {
+        console.log(ex.name + ": " + ex.message);
+    }
+};
+document.getElementById("projectbtn6").onclick = function () {
+    'use strict';
+    try {
+        location.href = "/~jbarrios2017/p6";
+    } catch (ex) {
+        console.log(ex.name + ": " + ex.message);
+    }
+};
+document.getElementById("projectbtn7").onclick = function () {
+    'use strict';
+    try {
+        location.href = "/~jbarrios2017/p7";
+    } catch (ex) {
+        console.log(ex.name + ": " + ex.message);
+    }
+};
